405a80e3869ff270800ab21e428d73f4925866db
sacamiso <sacamiso@unirioja.es>
2023-09-29 10:13:55 +0200

    actualizado prestamo provier impl

---
+++ libreriabk/src/main/java/providerImpl/PrestamoLibroProviderImpl.java
@@ -0,0 +1,40 @@
+package providerImpl;
+
+import java.util.List;
+
+import entity.PrestamoLibroEntity;
+import provider.PrestamoLibroProvider;
+
+public class PrestamoLibroProviderImpl implements PrestamoLibroProvider{
+
+	@Override
+	public List<PrestamoLibroEntity> listarPrestamosLibros() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public PrestamoLibroEntity anadirLibro(PrestamoLibroEntity prestamoLibro) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public PrestamoLibroEntity buscarPrestamoLibroId(int libroId, int prestamoId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public PrestamoLibroEntity editarPrestamoLibro(PrestamoLibroEntity prestamoLibro, int libroId, int prestamoId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deletePrestamoLibroById(int libroId, int prestamoId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- libreriabk/src/main/java/providerImpl/PrestamoProviderImpl.java
+++ libreriabk/src/main/java/providerImpl/PrestamoProviderImpl.java
@@ -1,47 +1,87 @@
 package providerImpl;
 
 import java.util.List;
+import java.util.Objects;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import entity.LibroEntity;
 import entity.PrestamoEntity;
+import entity.UsuarioEntity;
 import provider.PrestamoProvider;
+import repository.LibroRepository;
+import repository.PrestamoRepository;
 
+//faltan muchas validaciones
+@Service
 public class PrestamoProviderImpl implements PrestamoProvider{
+	
+	@Autowired
+	private PrestamoRepository prestamoRepository;
+	@Autowired
+	private LibroRepository libroRepository;
+	
+	private UsuarioProviderImpl usuarioProviderImpl;
 
 	@Override
 	public List<PrestamoEntity> listarPrestamos() {
-		// TODO Auto-generated method stub
-		return null;
+		return prestamoRepository.findAll();
 	}
 
 	@Override
 	public PrestamoEntity anadirPrestamo(PrestamoEntity prestamo) {
-		// TODO Auto-generated method stub
-		return null;
+		return this.prestamoRepository.save(prestamo);
 	}
 
 	@Override
 	public PrestamoEntity buscarPrestamoId(int prestamoId) {
-		// TODO Auto-generated method stub
-		return null;
+		return this.prestamoRepository.getReferenceById(prestamoId);
 	}
 
 	@Override
 	public PrestamoEntity editarPrestamo(PrestamoEntity prestamo, int prestamoId) {
-		// TODO Auto-generated method stub
-		return null;
+		PrestamoEntity prestamoDB = prestamoRepository.getReferenceById(prestamoId);
+
+		prestamoDB.setFechaDevolucion(prestamo.getFechaDevolucion());
+
+		if (Objects.nonNull(prestamo.getFechaPrestamo())) {
+			prestamoDB.setFechaPrestamo(prestamo.getFechaPrestamo());
+		}
+
+		UsuarioEntity usuAux = usuarioProviderImpl.buscarUsuarioId(prestamo.getId());
+		
+		if (Objects.nonNull(usuAux)) {
+			prestamoDB.setIdUsuario(usuAux.getId());
+		}
+
+		// No tengo claro que todas estas comprobaciones sean ncesarias o correctas
+		List<LibroEntity> libros = prestamo.getLibros();
+		LibroEntity libroAux;
+		boolean librosCorrectos = true;
+		for (LibroEntity lib : libros) {
+			libroAux = libroRepository.getReferenceById(lib.getId());
+			if (libroAux == null) {
+				librosCorrectos = false;
+			}
+		}
+		if (librosCorrectos) {
+			prestamoDB.setLibros(prestamo.getLibros());
+		}
+		
+		return prestamoRepository.save(prestamoDB);
 	}
 
 	@Override
 	public void deletePrestamoById(int prestamoId) {
-		// TODO Auto-generated method stub
+		this.prestamoRepository.deleteById(prestamoId);
 		
 	}
 
 	@Override
 	public List<LibroEntity> listarLibrosPrestamo(int prestamoId) {
-		// TODO Auto-generated method stub
-		return null;
+		PrestamoEntity prestamoBD = this.prestamoRepository.getReferenceById(prestamoId);
+        return prestamoBD.getLibros();
 	}
 
 	
