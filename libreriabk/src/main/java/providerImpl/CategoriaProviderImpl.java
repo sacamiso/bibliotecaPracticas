cb170f719e0ac0eda79ee690cbf65b5d63df8234
sacamiso <sacamiso@unirioja.es>
2023-09-29 10:29:08 +0200

    actualizado categoriaPrividerImpl pero le faltan cosas

--- libreriabk/src/main/java/providerImpl/CategoriaProviderImpl.java
+++ libreriabk/src/main/java/providerImpl/CategoriaProviderImpl.java
@@ -1,47 +1,79 @@
 package providerImpl;
 
 import java.util.List;
+import java.util.Objects;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import entity.AutorEntity;
 import entity.CategoriaEntity;
 import entity.LibroEntity;
 import provider.CategoriaProvider;
+import repository.CategoriaRepository;
+import repository.LibroRepository;
 
-public class CategoriaProviderImpl implements CategoriaProvider{
+//faltan muchas validaciones
+@Service
+public class CategoriaProviderImpl implements CategoriaProvider {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private LibroRepository libroRepository;
 
 	@Override
 	public List<CategoriaEntity> listarCategorias() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.categoriaRepository.findAll();
 	}
 
 	@Override
 	public CategoriaEntity anadirCategoria(CategoriaEntity categoria) {
-		// TODO Auto-generated method stub
-		return null;
+		return this.categoriaRepository.save(categoria);
 	}
 
 	@Override
 	public CategoriaEntity buscarCategoriaId(int categoriaId) {
-		// TODO Auto-generated method stub
-		return null;
+		return this.categoriaRepository.getReferenceById(categoriaId);
 	}
 
 	@Override
 	public CategoriaEntity editarCategoria(CategoriaEntity categoria, int categoriaId) {
-		// TODO Auto-generated method stub
-		return null;
+		CategoriaEntity categoriaDB = this.categoriaRepository.getReferenceById(categoriaId);
+
+		if (Objects.nonNull(categoria.getDescripcion()) && !"".equalsIgnoreCase(categoria.getDescripcion())) {
+			categoriaDB.setDescripcion(categoria.getDescripcion());
+		}
+		if (Objects.nonNull(categoria.getNombre()) && !"".equalsIgnoreCase(categoria.getNombre())) {
+			categoriaDB.setNombre(categoria.getNombre());
+			// Falta asegurarse que no existe ninguna categoria con ese mismo nombre
+		}
+
+		// No tengo claro que todas estas comprobaciones sean ncesarias o correctas
+		List<LibroEntity> libros = categoria.getListaLibros();
+		LibroEntity libroAux;
+		boolean librosCorrectos = true;
+		for (LibroEntity libro : libros) {
+			libroAux = libroRepository.getReferenceById(libro.getId());
+			if (libro == null) {
+				librosCorrectos = false;
+			}
+		}
+		if (librosCorrectos) {
+			categoriaDB.setListaLibros(libros);
+		}
+		return this.categoriaRepository.save(categoriaDB);
 	}
 
 	@Override
 	public void deleteCategoriaById(int categoriaId) {
-		// TODO Auto-generated method stub
-		
+		this.categoriaRepository.deleteById(categoriaId);
 	}
 
 	@Override
 	public List<LibroEntity> listarLibrosCategoria(int categoriaId) {
-		// TODO Auto-generated method stub
-		return null;
+		CategoriaEntity categoriaBD = this.categoriaRepository.getReferenceById(categoriaId);
+        return categoriaBD.getListaLibros();
 	}
 
 }
