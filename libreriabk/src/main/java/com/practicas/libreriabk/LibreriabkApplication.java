2264b47abb7203d4d04406ac803957b9dedc694a
sacamiso <sacamiso@unirioja.es>
2023-09-29 08:59:24 +0200

    prueba

---
+++ libreriabk/src/main/java/com/practicas/libreriabk/LibreriabkApplication.java
@@ -0,0 +1,13 @@
+package com.practicas.libreriabk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibreriabkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibreriabkApplication.class, args);
+	}
+
+}
---
+++ libreriabk/src/test/java/com/practicas/libreriabk/LibreriabkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practicas.libreriabk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibreriabkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
